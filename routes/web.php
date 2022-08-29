55e63bfdc3b8ef4625a992b7425a09db11187329
Luis Campos <campos.luis19@gmail.com>
2022-08-29 11:02:44 -0400

    Laravel Wallet Config
    Instalacion de Billetera y Migraciones

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix'  =>  '/api'], function()
+{
+    Route::post('/register',         [ApiController::class, 'register'])->name('/register');
+});
